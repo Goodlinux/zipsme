17105a66f8f7d913d3d65417950d1fa578f1d36a
Goodlinux <ludovic.maillet@gmail.com>
2021-03-04 22:34:45 +0100

    Update Info.php

--- Info.php
+++ Info.php
@@ -9,7 +9,7 @@ class Info {
 		$this->url_name = strtolower($url_name);
 		$query = "SELECT * FROM tbl_links WHERE url_name = '" . $this->url_name . "' LIMIT 1";
 		$result = $DbConnect->query($query);
-		$row =  mysqli_fetch_array($result);
+		$row =  mysqli_fetch_array($result);   
 		$this->url = $row['url'];
 		$this->type = $row['type'];
 		mysqli_close($DbConnect);
