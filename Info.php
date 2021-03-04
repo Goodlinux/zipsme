22caac82081e4a0edfe21ecf645eb9fc0e4655ec
Goodlinux <ludovic.maillet@gmail.com>
2021-03-04 21:04:30 +0100

    Update Info.php

--- Info.php
+++ Info.php
@@ -5,13 +5,14 @@ class Info {
 	public $type;
 	
 	function __construct($url_name) {
-		$this->url_name = $url_name;
+		$DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
+		$this->url_name = strtolower($url_name);
 		$query = "SELECT * FROM tbl_links WHERE url_name = '" . $this->url_name . "' LIMIT 1";
-		$result = mysql_query($query,$GLOBALS['DB']);
-		$row = mysql_fetch_assoc($result);
+		$result = $DbConnect->query($query);
+		$row =  mysqli_fetch_array($result);
 		$this->url = $row['url'];
 		$this->type = $row['type'];
-		
+		mysqli_close($DbConnect);
 	}
 	
 	function __get($name) {
