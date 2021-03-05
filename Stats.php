7c42c3a051761edbe6179c480b19c772faa58557
Goodlinux <ludovic.maillet@gmail.com>
2021-03-05 22:10:36 +0100

    Update Stats.php

--- Stats.php
+++ Stats.php
@@ -13,13 +13,16 @@ class Stats {
 	public $o_none = 0;
 	
 	function __construct($url_name) {
+		$DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
+		$url_name = strtolower($url_name);
 		$this->url_name = $url_name;
 		$query = "SELECT COUNT(url_name) AS urlCount FROM tbl_clicks WHERE url_name = '" . $this->url_name . "'";
-		$result = mysql_query($query,$GLOBALS['DB']);
-		$row = mysql_fetch_assoc($result);
+		$result = $DbConnect->query($query);
+		$row =  mysqli_fetch_array($result);  
 		$this->total_clicks = $row['urlCount'];	
 		$this->calcBrowsers();
 		$this->calcOS();
+		mysqli_close($DbConnect);
 	}
 	
 	function __get($name) {
@@ -31,23 +34,28 @@ class Stats {
 	}	
 	
 	function showClicks() {
+		$DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
 		$query = "SELECT click_time, COUNT(url_name) AS monthCount FROM tbl_clicks WHERE url_name = '" . $this->url_name . "' GROUP BY EXTRACT(MONTH FROM click_time) ORDER BY click_time DESC";
-		$result = mysql_query($query,$GLOBALS['DB']);
-		$row = mysql_fetch_assoc($result);
-		do {
+		$result = $DbConnect->query($query);
+		//$row =  mysqli_fetch_array($result); 
+		while ($row = mysqli_fetch_array($result)) 
+		 {
 			$month = strtotime($row['click_time']);
 			echo '<tr>' . "\n";
 			echo '<td class="border">' . date('F Y', $month) . '</td>' . "\n";
 			echo '<td class="border">' . $row['monthCount'] . '</td>' . "\n";
 			echo '</tr>' . "\n";
-		} while ($row = mysql_fetch_assoc($result));
+		} 
+		mysqli_close($DbConnect);
 	}
 	
 	function showReferrers() {
+		$DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
 		$query = "SELECT referrer, COUNT(referrer) AS refCount FROM tbl_clicks WHERE url_name = '" . $this->url_name . "' GROUP BY referrer ORDER BY refCount DESC";
-		$result = mysql_query($query,$GLOBALS['DB']);
-		$row = mysql_fetch_assoc($result);
-		do {
+		$result = $DbConnect->query($query);
+		//$row = mysql_fetch_assoc($result);
+		while ($row = mysqli_fetch_array($result)) 
+		{
 			$referrer = str_replace('http://', '',$row['referrer']);
 			$referrer = str_replace('www.','',$referrer);
 			if (strlen($referrer)==0) {
@@ -57,15 +65,17 @@ class Stats {
 			echo '<td class="border">' . prepOutputText($referrer) . '</td>' . "\n";
 			echo '<td class="border">' . $row['refCount'] . '</td>' . "\n";
 			echo '</tr>' . "\n";
-		} while ($row = mysql_fetch_assoc($result));
+		} 
+		mysqli_close($DbConnect);
 	}
 	
 	function calcBrowsers() {
+		$DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
 		$query = "SELECT user_agent FROM tbl_clicks WHERE url_name = '" . $this->url_name . "'";
-		$result = mysql_query($query,$GLOBALS['DB']);
+		$result = $DbConnect->query($query);
 		$row = mysql_fetch_assoc($result);
-		
-		do {
+		while ($row = mysqli_fetch_array($result)) 
+		{
 			$userAgent = strtolower($row['user_agent']);       
 			if (preg_match('/opera/', $userAgent)) {
 				$this->b_opera++;
@@ -78,15 +88,17 @@ class Stats {
 			} else {
 				$this->b_none++;
 			}
-		} while ($row = mysql_fetch_assoc($result));		
+		} 		
+		mysqli_close($DbConnect);
 	}
 	
 	function calcOS() {
+		$DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
 		$query = "SELECT user_agent FROM tbl_clicks WHERE url_name = '" . $this->url_name . "'";
-		$result = mysql_query($query,$GLOBALS['DB']);
-		$row = mysql_fetch_assoc($result);
-		
-		do {
+		$result = $DbConnect->query($query);
+		//$row = mysql_fetch_assoc($result);
+		while ($row = mysqli_fetch_array($result)) 
+		{
 			$userAgent = strtolower($row['user_agent']);       
 			if (preg_match('/linux/', $userAgent)) {
 				$this->o_linux++;
@@ -97,7 +109,8 @@ class Stats {
 			} else {
 				$this->o_none++;
 			}
-		} while ($row = mysql_fetch_assoc($result));		
+		} 
+		mysqli_close($DbConnect);
 	}		
 }
 ?>
