00e94bdafaa1baf3f5eda2a3c62e72159f8794f0
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-05-10 23:15:02 +0200

    Version 2.0
    Add LDAP functions

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <?php include('config.php'); ?>
+
+<?php 
+	if ( ! isset($COOKIE['zipsme-user']) ) {
+		setcookie('zipsme-user', '');
+	}
+	
+	if ( ! isset($COOKIE['zipsme-login']) ) {
+		setcookie('zipsme-login', 'no');
+	}
+?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="robots" content="index,follow" />
-<link type="text/css" rel="stylesheet" href="reset-fonts-grids.css" />
-<link type="text/css" rel="stylesheet" href="styles.css" />
-<title><?php echo SITE_NAME; ?> - Powered by Z.ips.ME</title>
-</head>
-<body>
-	<center>
-    <div id="container">
-        <div id="header"><h1><?php echo SITE_NAME; ?> - Powered by Z.ips.ME</h1></div>
-        <div id="content">
-        	<p>Z.ips.ME is a free program that allows you to use your website as your very own URL shortening service. </p>
-            <p>Download your copy at <a href="http://z.ips.me">http://z.ips.me</a></p>            
-            <p><a href="admin.php">Admin Login</a></p>
-        </div>
-        <div id="footer">Powered by <a href="http://z.ips.me">Z.ips.ME</a></div>
-	</div>
-    </center>
-</body>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="robots" content="index,follow" />
+		<link type="text/css" rel="stylesheet" href="reset-fonts-grids.css" />
+		<link type="text/css" rel="stylesheet" href="styles.css" />
+		<title><?php echo SITE_NAME; ?> - Powered by Z.ips.ME</title>
+	</head>
+	<META HTTP-EQUIV=Refresh CONTENT="0; URL=admin.php">
 </html>
