5c07cfc675c79ccf32b75a55920ca95e460e85d9
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-05-27 22:12:19 +0200

    Version 2.0

--- install.php
+++ install.php
@@ -5,11 +5,28 @@
 //create clicks table
 $DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
 
-$query = "CREATE TABLE IF NOT EXISTS tbl_clicks (click_id int(11) NOT NULL auto_increment,click_time datetime NOT NULL,url_name varchar(255) NOT NULL,referrer varchar(255) NOT NULL,user_agent varchar(255) NOT NULL,ip_address varchar(255) NOT NULL,  PRIMARY KEY  (click_id))";
-$result = $DbConnect->query($query);
+$query = "select * from tbl_links";
+if (!$DbConnect->query($query)) {
+    if ( $DbConnect->error == "1146" ) {
+      //#1146 - La table 'xxxx' n'existe pas
+	    printf( "Création de la table \n");
+	    $query = "CREATE TABLE tbl_links (url_name varchar(255) NOT NULL, url text NOT NULL, user varchar(255), type varchar(255) NOT NULL, active char(1) NOT NULL, PRIMARY KEY  (url_name))";
+	    $result = $DbConnect->query($query);
+    }
+	}
+else {
+	$query = "select user from tbl_links";
+	if ( !$DbConnect->query($query) ) {
+		if ( $DbConnect->errno == "1054" ) {
+      //#1054 - Champ 'user' inconnu dans field list
+			printf( "Ajout de la colonne 'user' \n");
+			$query = "alter table tbl_links ADD COLUMN user varchar(255)";
+			$result = $DbConnect->query($query);
+		}
+	}
+}
 
-//create links table
-$query = "CREATE TABLE IF NOT EXISTS tbl_links (url_name varchar(255) NOT NULL,url text NOT NULL,user varchar(255), type varchar(255) NOT NULL,active char(1) NOT NULL, PRIMARY KEY  (url_name))";
+$query = "CREATE TABLE IF NOT EXISTS tbl_clicks (click_id int(11) NOT NULL auto_increment,click_time datetime NOT NULL,url_name varchar(255) NOT NULL,referrer varchar(255) NOT NULL,user_agent varchar(255) NOT NULL,ip_address varchar(255) NOT NULL,  PRIMARY KEY  (click_id))";
 $result = $DbConnect->query($query);
 
 mysqli_close($DbConnect);
