74740916e65441fd59d8e02cd27025b2f9b89cf3
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-05-28 20:04:00 +0200

    Version 2.0.0

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php include('config.php'); ?>
-
 <?php 
 	if ( ! isset($COOKIE['zipsme-user']) ) {
 		setcookie('zipsme-user', '');
@@ -9,8 +8,6 @@
 		setcookie('zipsme-login', 'no');
 	}
 ?>
-
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
