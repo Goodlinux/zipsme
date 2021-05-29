62822443d7e50fc51164f8997d1c9e03b1ebbceb
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-05-29 23:18:53 +0200

    Version 2.0.1

--- redirect.php
+++ redirect.php
@@ -4,6 +4,7 @@ include('config.php');
 $url_name = prepQueryText($_GET['url_name']);
 
 if (linkExists($url_name)) {
+	// name exist, so add a new click for stats
 	$referrer = $_SERVER['HTTP_REFERER'];
 	$user_agent = $_SERVER['HTTP_USER_AGENT'];
 	$ip_address = getIpAddress();	
@@ -11,6 +12,7 @@ if (linkExists($url_name)) {
 	insertClick($url_name, $referrer, $user_agent, $ip_address);
 	redirectClick($url_name);
 } else {
+	// propose to create a new link
 	echo '<script type="text/javascript">window.location = "admin.php?newlink=' . $url_name .'" </script>';
 }
 ?>
