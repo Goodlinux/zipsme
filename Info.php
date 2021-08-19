e0378a74756d7d7bc582b8852e65e2e7ba743419
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-19 22:11:51 +0200

    Update Info.php

--- Info.php
+++ Info.php
@@ -5,7 +5,7 @@ class Info {
 	public $type;
 	function __construct($url_name) {
 		$this->url_name = strtolower($url_name);
-		$DbConnect = sqlConnect();
+		$DbConnect = mysqli_connect(ZIPSME_DB_HOST, ZIPSME_DB_USER, ZIPSME_DB_PASSWORD, ZIPSME_DB_NAME);
 		$query = "SELECT * FROM tbl_links WHERE url_name = '" . $this->url_name . "' LIMIT 1";
 		$result = $DbConnect->query($query);
 		$row = mysqli_fetch_array($result); 
