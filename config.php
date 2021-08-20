f24bbc168dd390807055e247b256310aa5c428af
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-20 21:21:55 +0200

    Version 2.2

--- config.php
+++ config.php
@@ -1,13 +1,14 @@
 <?php
 //This is where you enter in your information
-//define('ZIPSME_DB_USER', 'database_user'); //Your database user name
-//define('ZIPSME_DB_PASSWORD', 'database_password'); //Your database user password
-//define('ZIPSME_DB_NAME', 'database_name'); //Your database name
-//define('ZIPSME_DB_HOST', 'localhost:3306'); //Url of the mysql server ex : 192.168.10.105:3306 OR localhost:3306
-//define('SITE_NAME', 'Your Site'); //The name of your site
-//define('SITE_URL', 'http://www.yoursite.com/zipsme');  //The full URL of the site where Z.ips.ME is installed (including trailing slash)
-//define('LDAP_SRV', 'ldap://localhost:389');  // serveur LDAP   //port du serveur ldap  ldaps == 636  lsap == 389
-//define('LDAP_RACINE', 'dc=domain,dc=extention');  // ldap racine
+
+define('DB_USER', $_SERVER["DB_USER"]); //Your database user name
+define('DB_PASSWORD', $_SERVER["DB_PASSWORD"]); //Your database user password
+define('DB_NAME', $_SERVER["DB_NAME"]); //Your database name
+define('DB_SERVER', $_SERVER["DB_SERVER"]); //Url of the mysql server ex : 192.168.10.105:3306 OR localhost:3306
+define('SITE_NAME', 'Your Site'); //The name of your site
+define('SITE_URL', 'http://www.yoursite.com/zipsme');  //The full URL of the site where Z.ips.ME is installed (including trailing slash)
+define('LDAP_SRV', 'ldap://localhost:389');  // serveur LDAP   //port du serveur ldap  ldaps == 636  lsap == 389
+define('LDAP_RACINE', 'dc=domain,dc=extention');  // ldap racine
 
 
 //You shouldn't need to modify anything below this.
@@ -19,7 +20,7 @@ define ('IS_ENV_PRODUCTION', true);
 //include common functions
 include('functions.php');
 
-$DbConnect = mysqli_connect($_SERVER["DB_SERVER"], $_SERVER["DB_USER"], $_SERVER["DB_PASSWORD"], $_SERVER["DB_NAME"]);
+$DbConnect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
 if (!$DbConnect) 
    { die("Could not connect. Please make sure that you have configured the config.php file correctly : " . mysqli_error());  }
 
