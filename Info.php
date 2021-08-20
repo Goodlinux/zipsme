7247264d63c99f1bc787fa4532f34ba552ce0c45
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-20 14:32:19 +0200

    Version 2.2

--- Info.php
+++ Info.php
@@ -5,7 +5,7 @@ class Info {
 	public $type;
 	function __construct($url_name) {
 		$this->url_name = strtolower($url_name);
-		$DbConnect = sqlConnect();
+		$DbConnect = mysqli_connect($_SERVER["DB_SERVER"], $_SERVER["DB_USER"], $_SERVER["DB_PASSWORD"], $_SERVER["DB_NAME"]);
 		$query = "SELECT * FROM tbl_links WHERE url_name = '" . $this->url_name . "' LIMIT 1";
 		$result = $DbConnect->query($query);
 		$row = mysqli_fetch_array($result); 
