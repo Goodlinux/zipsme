b4c9d59a7b25dac738e677f62bf04a5af92fb001
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-22 11:49:49 +0200

    Version 2.2

--- config.php
+++ config.php
@@ -1,20 +1,20 @@
 <?php
-//This is where you enter in your information
 
-define('DB_USER', $_SERVER["DB_USER"]); //Your database user name
-define('DB_PASSWORD', $_SERVER["DB_PASSWORD"]); //Your database user password
-define('DB_NAME', $_SERVER["DB_NAME"]); //Your database name
-define('DB_SERVER', $_SERVER["DB_SERVER"]); //Url of the mysql server ex : 192.168.10.105:3306 OR localhost:3306
-define('SITE_NAME', 'Your Site'); //The name of your site
-define('SITE_URL', 'http://www.yoursite.com/zipsme');  //The full URL of the site where Z.ips.ME is installed (including trailing slash)
-define('LDAP_SRV', 'ldap://localhost:389');  // serveur LDAP   //port du serveur ldap  ldaps == 636  lsap == 389
-define('LDAP_RACINE', 'dc=domain,dc=extention');  // ldap racine
-
-
-//You shouldn't need to modify anything below this.
-
+// configure error reporting options
 //set true if production environment else false for development
+
 define ('IS_ENV_PRODUCTION', true); 
+error_reporting(E_ALL ^ E_NOTICE);
+ini_set('display_errors', !IS_ENV_PRODUCTION);
+ini_set('error_log', '/var/www/zipsme/log/phperror.txt');
+// set time zone to use date/time functions without warnings
+date_default_timezone_set('Europe/Paris');
+
+
+//This is where you enter in your information
+include('Const.php');
+
+
 //establish a connection to the database server
 
 //include common functions
@@ -31,15 +31,5 @@ mysqli_close($DbConnect);
 spl_autoload_register(function ($class) 
 { include $class . '.php';});
 
-
-
-// set time zone to use date/time functions without warnings
-date_default_timezone_set('Europe/Paris');
-
-
-// configure error reporting options
-error_reporting(E_ALL ^ E_NOTICE);
-ini_set('display_errors', !IS_ENV_PRODUCTION);
-ini_set('error_log', 'log/phperror.txt');
 
 ?>
