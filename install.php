a1fefef13beb06688ddf3244847661fb92f1cf1f
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-22 16:16:37 +0200

    Version 2.2

--- install.php
+++ install.php
@@ -29,6 +29,10 @@ else {
 $query = "CREATE TABLE IF NOT EXISTS tbl_clicks (click_id int(11) NOT NULL auto_increment,click_time datetime NOT NULL,url_name varchar(255) NOT NULL,referrer varchar(255) NOT NULL,user_agent varchar(255) NOT NULL,ip_address varchar(255) NOT NULL,  PRIMARY KEY  (click_id))";
 $result = $DbConnect->query($query);
 
+$query = "CREATE TABLE IF NOT EXISTS users (user_id varchar(255) NOT NULL, username varchar(255) NOT NULL,  PRIMARY KEY (user_id))";
+$result = $DbConnect->query($query);
+
+
 mysqli_close($DbConnect);
 
 echo 'Z.ips.ME installed successfully!  You can now log in to your Admin page <a href="admin.php">here</a>';
