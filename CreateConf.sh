b92f59611362cd60c729c9e49ac305ac0f6a5a70
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-22 11:57:44 +0200

    Version 2.2

--- CreateConf.sh
+++ CreateConf.sh
@@ -5,6 +5,6 @@ echo "define('DB_NAME', "$DB_NAME");"         >> /var/www/zipsme/Const.php
 echo "define('DB_SERVER', "$DB_SERVER");"     >> /var/www/zipsme/Const.php 
 echo "define('SITE_NAME', "$SITE_NAME");"     >> /var/www/zipsme/Const.php 
 echo "define('SITE_URL', "$SITE_URL");"       >> /var/www/zipsme/Const.php 
-echo "define('LDAP_SRV', "$LDAPSRV");"        >> /var/www/zipsme/Const.php 
+echo "define('LDAP_SRV', "$LDAP_SRV");"       >> /var/www/zipsme/Const.php 
 echo "define('LDAP_RACINE', 'dc="$LDAP_DOMAIN",dc="$LDAP_EXT"');"  >> /var/www/zipsme/Const.php 
 echo "?>"                                     >> /var/www/zipsme/Const.php 
