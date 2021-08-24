0d2a46beb0d1de140dbc5f08fbf95c60cf4da962
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-24 11:36:06 +0200

    Version 2.3

--- redirect.php
+++ redirect.php
@@ -6,10 +6,16 @@ $url_name = prepQueryText($_GET['url_name']);
 if (linkExists($url_name)) {
 	// name exist, so add a new click for stats
 	$referrer = $_SERVER['HTTP_REFERER'];
+	$browserInfo = new BrowserDetection();
+	$browserName = $browserInfo->getBrowser();
+	$osName = $browserInfo->getPlatform();
 	$user_agent = $_SERVER['HTTP_USER_AGENT'];
 	$ip_address = getIpAddress();	
 //	echo $url_name . ":" . $referrer . ":" . $user_agent . ":" .  $ip_address;
-	insertClick($url_name, $referrer, $user_agent, $ip_address);
+	echo $url_name . ":" . $osName . ":" . $browserName . ":" .  $ip_address;
+//	insertClick($url_name, $referrer, $user_agent, $ip_address);
+	insertClick($url_name, $osName, $browserName, $ip_address);
+	
 	redirectClick($url_name);
 } else {
 	// propose to create a new link
