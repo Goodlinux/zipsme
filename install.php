96183441419c8d95a81d6921cd2e57d2d850b786
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-24 11:50:55 +0200

    Version 2.3

--- install.php
+++ install.php
@@ -9,11 +9,11 @@ $query = "select * from tbl_links";
 if (!$DbConnect->query($query)) {
     if ( $DbConnect->error == "1146" ) {
       //#1146 - La table 'xxxx' n'existe pas
-	    printf( "Création de la table \n");
+	    printf( "Création de la table Links \n");
 	    $query = "CREATE TABLE tbl_links (url_name varchar(255) NOT NULL, url text NOT NULL, user varchar(255), type varchar(255) NOT NULL, active char(1) NOT NULL, PRIMARY KEY  (url_name))";
 	    $result = $DbConnect->query($query);
     }
-	}
+}
 else {
 	$query = "select user from tbl_links";
 	if ( !$DbConnect->query($query) ) {
@@ -26,9 +26,43 @@ else {
 	}
 }
 
-$query = "CREATE TABLE IF NOT EXISTS tbl_clicks (click_id int(11) NOT NULL auto_increment,click_time datetime NOT NULL,url_name varchar(255) NOT NULL,referrer varchar(255) NOT NULL,user_agent varchar(255) NOT NULL,ip_address varchar(255) NOT NULL,  PRIMARY KEY  (click_id))";
-$result = $DbConnect->query($query);
 
+//create links table
+
+$query = "select * from tbl_clicks";
+if (!$DbConnect->query($query)) {
+	if ( $DbConnect->error == "1146" ) {
+      		//#1146 - La table 'xxxx' n'existe pas
+		printf("Création de la table Clicks \n";
+		$query = "CREATE TABLE IF NOT EXISTS tbl_clicks (click_id int(11) NOT NULL auto_increment,click_time datetime NOT NULL,url_name varchar(255) NOT NULL,referrer varchar(255) NOT NULL,user_agent varchar(255) NOT NULL,ip_address varchar(255) NOT NULL,  PRIMARY KEY  (click_id))";
+		$result = $DbConnect->query($query);
+	}
+}
+else {
+	// Rename column referrer in os 
+	$query = "select referrer from tbl_clicks";
+	if ( !$DbConnect->query($query) ) {
+		if ( $DbConnect->errno == "1054" ) {
+      		//#1054 - Champ 'referrer' inconnu dans field list
+			printf( "Renommage de la colonne 'referrer' en 'os' \n");
+			$query = "alter table tbl_links MODIFY COLUMN 'referrer' 'os' varchar(255)";
+			$result = $DbConnect->query($query);
+		}
+	}
+	// Rename column user_agent  in browser 
+	$query = "select user_agent from tbl_clicks";
+	if ( !$DbConnect->query($query) ) {
+		if ( $DbConnect->errno == "1054" ) {
+      		//#1054 - Champ 'user_agent' inconnu dans field list
+			printf( "Renommage de la colonne 'user_agent ' en 'browser' \n");
+			$query = "alter table tbl_links MODIFY COLUMN 'user_agent' 'browser' varchar(255)";
+			$result = $DbConnect->query($query);
+		}
+	}
+}
+
+		       
+// create table users to store id for cookies purpose		       
 $query = "CREATE TABLE IF NOT EXISTS users (user_id varchar(255) NOT NULL, username varchar(255) NOT NULL,  PRIMARY KEY (user_id))";
 $result = $DbConnect->query($query);
 
