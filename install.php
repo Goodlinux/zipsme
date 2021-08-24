93ec39bcb58aee600e1f1ba860487840ebe7f331
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-24 19:53:19 +0200

    Version 2.3

--- install.php
+++ install.php
@@ -7,7 +7,7 @@ $DbConnect = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWORD, DB_NAME);
 
 $query = "select * from tbl_links";
 if (!$DbConnect->query($query)) {
-    if ( $DbConnect->error == "1146" ) {
+    if ( $DbConnect->errno == "1146" ) {
       //#1146 - La table 'xxxx' n'existe pas
 	    echo  "Création de la table Links \n";
 	    $query = "CREATE TABLE tbl_links (url_name varchar(255) NOT NULL, url text NOT NULL, user varchar(255), type varchar(255) NOT NULL, active char(1) NOT NULL, PRIMARY KEY  (url_name))";
@@ -29,7 +29,7 @@ else {
 //create links table
 $query = "select * from tbl_clicks";
 if (!$DbConnect->query($query)) {
-	if ( $DbConnect->error == "1146" ) {
+	if ( $DbConnect->errno == "1146" ) {
       		//#1146 - La table 'xxxx' n'existe pas
 		echo "Création de la table Clicks \n";
 		$query = "CREATE TABLE IF NOT EXISTS tbl_clicks (click_id int(11) NOT NULL auto_increment,click_time datetime NOT NULL,url_name varchar(255) NOT NULL,referrer varchar(255) NOT NULL,user_agent varchar(255) NOT NULL,ip_address varchar(255) NOT NULL,  PRIMARY KEY  (click_id))";
