2decf83af96da05ec68431e761d5d8f16fe62449
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2021-08-27 22:34:45 +0200

    Version 2.3

--- BrowserDetection.php
+++ BrowserDetection.php
@@ -7,13 +7,13 @@ class BrowserDetection {
     private $_basic_browser = array (
               'Trident\/7.0' => 'Internet Explorer 11', 'Beamrise' => 'Beamrise', 'Opera' => 'Opera', 'OPR' => 'Opera',
               'Shiira' => 'Shiira', 'Chimera' => 'Chimera', 'Phoenix' => 'Phoenix', 'Firebird' => 'Firebird', 'Camino' => 'Camino',
-              'Netscape' => 'Netscape', 'OmniWeb' => 'OmniWeb', 'Konqueror' => 'Konqueror', 'icab' => 'iCab', 'Lynx' => 'Lynx',
+              'Netscape' => 'Netscape', 'OmniWeb' => 'OmniWeb', 'Konqueror' => 'Konqueror', 'icab' => 'iCab', 'Lynx' => 'Lynx', 'Edg' => 'Edge',  
               'Links' => 'Links', 'hotjava' => 'HotJava', 'amaya' => 'Amaya', 'IBrowse' => 'IBrowse', 'iTunes' => 'iTunes', 'Silk' => 'Silk',
               'Dillo' => 'Dillo', 'Maxthon' => 'Maxthon', 'Arora' => 'Arora', 'Galeon' => 'Galeon', 'Iceape' => 'Iceape', 'Iceweasel' => 'Iceweasel',
               'Midori' => 'Midori', 'QupZilla' => 'QupZilla', 'Namoroka' => 'Namoroka', 'NetSurf' => 'NetSurf', 'BOLT' => 'BOLT', 'EudoraWeb' => 'EudoraWeb',
               'shadowfox' => 'ShadowFox', 'Swiftfox' => 'Swiftfox', 'Uzbl' => 'Uzbl', 'UCBrowser' => 'UCBrowser', 'Kindle' => 'Kindle', 'wOSBrowser' => 'wOSBrowser',
               'Epiphany' => 'Epiphany', 'SeaMonkey' => 'SeaMonkey', 'Avant Browser' => 'Avant Browser', 'Firefox' => 'Firefox', 'Chrome' => 'Google Chrome',
-              'MSIE' => 'Internet Explorer', 'Internet Explorer' => 'Internet Explorer', 'Safari' => 'Safari', 'Mozilla' => 'Mozilla', 'Edg' => 'Edge'  
+              'MSIE' => 'Internet Explorer', 'Internet Explorer' => 'Internet Explorer', 'Safari' => 'Safari', 'Mozilla' => 'Mozilla' 
               );
     private $_basic_platform = array(
               'windows' => 'Windows', 'iPad' => 'iPad', 'iPod' => 'iPod', 'iPhone' => 'iPhone', 'mac' => 'Apple', 'android' => 'Android', 'linux' => 'Linux',
