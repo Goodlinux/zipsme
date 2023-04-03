c0caeecbe5918ae356a49e90c2c63eaa876ac9f4
Goodlinux <880813+Goodlinux@users.noreply.github.com>
2023-04-03 18:46:20 +0200

    V3
    Implementation of Synology SSO

--- CreateConf.sh
+++ CreateConf.sh
@@ -5,7 +5,7 @@ echo "define('DB_NAME', '"$DB_NAME"');"         >> /var/www/zipsme/Const.php
 echo "define('DB_SERVER', '"$DB_SERVER"');"     >> /var/www/zipsme/Const.php 
 echo "define('SITE_NAME', '"$SITE_NAME"');"     >> /var/www/zipsme/Const.php 
 echo "define('SITE_URL', '"$SITE_URL"');"       >> /var/www/zipsme/Const.php 
-echo "define('LDAP_SRV', '"$LDAP_SRV"');"       >> /var/www/zipsme/Const.php 
-echo "define('LDAP_RACINE', 'cn=users,dc="$LDAP_DOMAIN",dc="$LDAP_EXT"');"  >> /var/www/zipsme/Const.php 
 echo "define('SRV_NAME', '"$SRV_NAME"');"       >> /var/www/zipsme/Const.php 
+echo "define('SERVEUR_SSO', '"$SERVEUR_SSO"');" >> /var/www/zipsme/Const.php
+echo "define('APPID_SSO', '"$APPID_SSO"');"         >> /var/www/zipsme/Const.php
 echo "?>"                                       >> /var/www/zipsme/Const.php 
